235e5297714fd716415aa146769ebd4542103988
Umami <karmandrug@gmail.com>
2024-03-07 19:19:25 +0200

    Show loading skeletons

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-
       <App />
     </ChakraProvider>
   </React.StrictMode>
